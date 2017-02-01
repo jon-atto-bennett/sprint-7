6d7f6e7d49997b06dd8159a4826152b0fd4de042
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-02-02 11:52:35 +1300

    eloquent js

---
+++ eloquent.js
@@ -0,0 +1,47 @@
+//Minimum
+
+function min(val1, val2){
+  if(val1<=val2){
+    return val1
+  }
+  else {
+    return val2
+  }
+};
+
+console.log(min(0, 10));
+
+//recursion
+
+function isEven(num) {
+  if(num<0){
+      num*= -1;
+  }
+  if(num == 0){
+    return true;
+  }
+  else if(num == 1){
+      return false;
+  }
+  else {
+     return isEven(num - 2);
+  }
+};
+
+console.log(isEven(-1));
+
+//Bean Counting
+
+//Count Bs
+
+function countBs(string){
+    var count = 0;
+    for(var i=0; i<string.length; i++) {
+       if(string.charAt(i)== "B") {
+           count++;
+       };
+    };
+    return count;
+};
+
+console.log(countBs("BBC"));
