85c58f888d9ea0df918ba703dadcbd4bd4839601
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-02-02 11:54:52 +1300

    eloquent js update

--- eloquent.js
+++ eloquent.js
@@ -45,3 +45,17 @@ function countBs(string){
 };
 
 console.log(countBs("BBC"));
+
+//Count Characters
+
+function countChar(string, char){
+    var count = 0;
+    for(var i=0; i<string.length; i++) {
+       if(string.charAt(i)== char) {
+           count++;
+       };
+    };
+    return count;
+};
+
+console.log(countChar("kakkerlak", "k"));
